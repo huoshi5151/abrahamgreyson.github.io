2a1ad0c309a5d9b024617060e68b36b285fc1a9c
东方孤思子（Paris·QianSen） <qiansen1386@gmail.com>
2014-10-27 03:23:36 +0800

    try Google analytics

--- layouts/main.php
+++ layouts/main.php
@@ -32,6 +32,14 @@ Yii::$app->language = 'zh-CN';
     <?= Html::csrfMetaTags() ?>
     <?php $this->head() ?>
     <title><?= Html::encode($this->context->pageTitle) ?></title>
+    <script>
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+        ga('create', 'UA-47725881-2', 'auto');
+        ga('send', 'pageview');
+</script><!--Google Analytics-->
 </head>
 <body>
 
